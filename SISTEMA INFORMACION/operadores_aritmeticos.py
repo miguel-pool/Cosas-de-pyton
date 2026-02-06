9653fa3d45ccd1c9deadf4b835fa1cf78a4eb4bc
Miguel <meduardo@gmail.com>
2026-02-06 11:21:10 -0600

    comparacion y aritmeticos

---
+++ SISTEMA INFORMACION/operadores_aritmeticos.py
@@ -0,0 +1,30 @@
+a = 10
+b = 5
+
+#SUMA +
+resultado_su = a + b
+print("La suma de esto es",resultado_su)
+
+#RESTA -
+resultado_res = a - b
+print("La resta de estos dos numeros es ",resultado_res)
+
+#DIVISION /
+resultado_div = a / b
+print("La respuesta al dividir es ",resultado_div)
+
+#MULTIPLICACION *
+resultado_mul = a * b
+print("La multiplicacion de estos dos nmeros es ",resultado_mul)
+
+#DIVISION ENTERA // (division siempre te tira flotante)
+resultado_div_ent = a // b
+print("La division entera de estos 2 numeros es ",resultado_div_ent)
+
+#MODULO %
+resultado_modu = a % b
+print("El modulo de esta operacion es ",resultado_modu)
+
+#POTENCIA **
+resultado_poten = a ** b
+print("La potencia es ",resultado_poten)
---
+++ SISTEMA INFORMACION/operadores_compara.py
@@ -0,0 +1,27 @@
+a = 5
+b = 10
+
+#Mayor que
+mayorque = a > b
+print("esto sera un resultado ",mayorque)
+
+#Menor que
+menorque = a < b
+print("esto sera un resultado ",menorque)
+
+#Igual a
+igualque = a == b
+print("esto sera un resultado ",igualque)
+
+#Diferente de
+diferente = a != b
+print("esto sera un resultado ",diferente)
+
+#Menor o igual
+menor_igual = a <= b
+print("esto sera un resultado ",menor_igual)
+
+#Mayor o igual
+mayor_igual = a >= b
+print("esto sera un resultado ",mayor_igual)
+
